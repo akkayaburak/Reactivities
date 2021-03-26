c897029c2f6188c744553e2da83a223d46fcab8d
Burak Akkaya <burak.akkaya@tcm.com.tr>
2021-03-26 12:54:10 +0300

    requireds setup

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,11 @@ namespace API
             services.AddDbContext<DataContext>(opt =>{
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(options => {
+                options.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +57,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
