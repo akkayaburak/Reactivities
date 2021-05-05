cf65e5f69fe3cc7ddbda212ddfcd4da1b99f7532
Burak Akkaya <burak.akkaya@tcm.com.tr>
2021-05-05 21:04:14 +0300

    error handling

--- client-app/src/app/layout/navbar.tsx
+++ client-app/src/app/layout/navbar.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
 
-export default function NavBar(){
+export default function NavBar() {
     return (
         <Menu inverted fixed='top'>
             <Container>
                 <Menu.Item as={NavLink} to='/' exact header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
+                    <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
                     Reactivities
                 </Menu.Item>
-                <Menu.Item as={NavLink} to='/activities' name='Activities'/>
+                <Menu.Item as={NavLink} to='/activities' name='Activities' />
+                <Menu.Item as={NavLink} to='/errors' name='Errors' />
                 <Menu.Item>
-                    <Button as={NavLink} to='/createActivity' positive content='Create Activity'/>
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>
             </Container>
         </Menu>
